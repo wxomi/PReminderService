9ee616679f5617664f29b8e560c969ccaddee0f3
wxomi <samsepi0I@outlook.com>
2023-04-10 02:58:17 +0530

    Rabbitmq cloud working subscriber ,moved to postgre

---
+++ src/utils/job.js
@@ -0,0 +1,29 @@
+const cron = require("node-cron");
+const sender = require("../config/emailConfig");
+const emailService = require("../services/email-service");
+
+const setupJobs = () => {
+  cron.schedule("*/15 * * * *", async () => {
+    const response = await emailService.fetchPendingEmail();
+    response.forEach((email) => {
+      sender.sendMail(
+        {
+          to: email.recepientEmail,
+          subject: email.subject,
+          text: email.content,
+        },
+        async (err, data) => {
+          if (err) {
+            console.log(err);
+          } else {
+            await emailService.updateTicket(email.id, {
+              status: "SUCCESS",
+            });
+          }
+        }
+      );
+    });
+  });
+};
+
+module.exports = setupJobs;
